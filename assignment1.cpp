f1f55c0d886b922ded131049d4c16e148eb5df82
Hillary <casscainbatgirl@gmail.com>
2012-01-18 18:21:11 -0500

    Added a node.h

--- assignment1.cpp
+++ assignment1.cpp
@@ -7,36 +7,6 @@
 #include <iostream>
 using namespace std;
 
-class node{
-public:
-    node(node &parent);
-    node();
-    ~node();
-private:
-    node parent;
-    node[] children;
-    int cost;
-    //state
-    //action
-    addChild(node* child);
-}
-
-node::node(){
-    this->parent = NULL;
-}
-
-node::node(node &parent){
-    this->parent = &parent;
-}
-
-node::addChild(node &child){
-    for (int i=0; i < this->children.length; i++) {
-        if(this->children[i] == NULL){
-            this->children[i] = &child;
-        }
-    }
-}
-
 int main(){
     node root = new node();
 }
---
+++ node.h
@@ -0,0 +1,45 @@
+//
+//  node.h
+//
+//  Created by Hillary Fotino and Eric Finn on 1/18/12.
+//
+
+#ifndef _node_h
+#define _node_h
+
+/*
+ *  Class for nodes for tree creation and traversal
+ */
+
+class node{
+public:
+    node(node &parent);
+    node();
+    ~node();
+private:
+    node parent;
+    node[] children;
+    int cost;
+    //state
+    //action
+    addChild(node* child);
+}
+
+node::node(){
+    this->parent = NULL;
+}
+
+node::node(node &parent){
+    this->parent = &parent;
+}
+
+addChild(node &child){
+    for (int i=0; i < this->children.length; i++) {
+        if(this->children[i] == NULL){
+            this->children[i] = &child;
+        }
+    }
+}
+
+
+#endif
