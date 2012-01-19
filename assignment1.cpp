f27d672c9fed56c5ef6b66e4357eb2d93865cfa2
Hillary <casscainbatgirl@gmail.com>
2012-01-18 20:21:34 -0500

    Started updating the main file to call the 3 search algorithms

--- assignment1.cpp
+++ assignment1.cpp
@@ -12,3 +12,33 @@ using namespace std;
 int main(){
     node root = new node();
 }
+
+
+/* Function name: breadthFirstSearch
+ * Function purpose: function to traverse the tree using the breadth first search algorithm
+ * Return:
+ *  void
+ */
+void breadthFirstSearch(){
+    cout << "Breadth First Search" << endl;
+}
+
+
+/* Function name: depthFirstSearch
+ * Function purpose: function to traverse the tree using the depth first search algorithm
+ * Return:
+ *  void
+ */
+void depthFirstSearch(){
+    cout << "Depth First Search" << endl;
+}
+
+
+/* Function name: aStarSearch
+ * Function purpose: function to traverse the tree using the A* search algorithm
+ * Return:
+ *  void
+ */
+void aStarSearch(){
+    cout << "A* search" << endl;
+}
