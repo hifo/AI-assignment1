f1aa88ea05b726f55b184078ff970e89b8d9e330
Eric Finn <ericfinn.166@gmail.com>
2012-01-19 17:55:56 -0500

    Added expand function and helpers

--- node.h
+++ node.h
@@ -6,57 +6,96 @@
 //  Created by Hillary Fotino and Eric Finn on 1/18/12.
 //
 
-#ifndef _node_h
-#define _node_h
-
 /*
  *  Class for nodes for tree creation and traversal
  */
 
 class node{
 public:
-    node(node &parent);
-    node();
+    node(state &_currState); //constructor for root node
+    node(node *_parent, state &_currState, int _cost); //constructor for child nodes
     ~node();
+	void expand();
+	int getDepth();
 private:
+    void addChild(node &child);
+	void addNextState(state &nextState);
     node *parent;
-    vector<node> &children;
+    vector<node*> children;
     int cost;
     int depth;
     state currState;
-    void addChild(vector<node> child);
-}
+};
 
 /* Function name: node
  * Function purpose: constructor for node class, used for the root which has no parent
  * Parameters: 
- *      none
+ *      state &_currState: the state associated with the node being constructed
  */
-node::node(){
+node::node(state &_currState){
     this->parent = NULL;
     this->cost = 0;
     this->depth = 0;
+	this->currState = _currState;
 }
 
 
 /* Function name: node
- * Function purpose: constructor for node class
+ * Function purpose: constructor for node class for child nodes
  * Parameters: 
- *      node &parent: the node which is the parent node for the node being constructed
+ *      node &_parent: the node which is the parent node for the node being constructed
+ *		state &_currState: the state associated with the node being constructed
+ *		int _cost: the cost to get from the parent node to the node being constructed
  */
-node::node(node &parent){
-    this->parent = &parent;
-    this->depth = this->parent-depth+1;
+node::node(node &_parent, state &_currState, int _cost){
+    this->parent = &_parent;
+	this->cost = _cost;
+    this->depth = this->parent->getDepth()+1;
+	this->currState = _currState;
 }
 
+/* Function name: ~node
+ * Function purpose: destructor for node class
+ */
+node::~node() {
+	for(vector<node*>::iterator currChild = children.begin(); currChild < children.end(); currChild++) {
+		delete *currChild;
+	}
+}
 
 /* Function name: addChild
  * Function purpose: adds a child to a given node
+ * Parameters:
+ *		node &child: node to add as a child to this node
  * Returns: void
  */
 void node::addChild(node &child){
-    this.push_back(&child);
+    this->children.push_back(&child);
 }
 
+/* Function name: expand
+ * Function purpose: create and add children states to this node
+ * Returns: void
+ */
+void node::expand() {
+	vector<state*> nextStates;
+	vector<int> costs;
+	int nextStates = 0;
+	nextStates = currState->getPossibleNextStates(nextStates, costs);
+	//iterate through nextStates, adding a node and its corresponding cost for each state
+	for(int index = 0; index < nextStates; index++) {
+		addNextState(nextStates[index], costs[index]);
+	}
+}
 
-#endif
+/* Function name: addNextState
+ * Function purpose: Add a possible next state as a child node to the tree
+ * Parameters:
+ *		state &nextState: the possible next state for which to create a node
+ *		int cost: the cost to get from this state to the possible next state
+ * Returns: void
+ */
+void node::addNextState(state &nextState, int cost) {
+	node *child = new node(*this, nextState, cost);
+	this->children.push_back(child);
+}
